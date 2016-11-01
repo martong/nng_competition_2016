6fe47b5cbc8d99f3567e61c1122f72cfbd3af689
Gabor Marton <martongabesz@gmail.com>
2016-11-01 11:33:31 +0100

    Add RlogN impl, initial

--- Schrodinger/solution.cpp
+++ Schrodinger/solution.cpp
@@ -23,6 +23,45 @@ V linear(size_t NumberOfBalls, size_t RadioactiveBalls, TestFun testFun) {
     return res;
 }
 
+struct TestFunW {
+    TestFun testFun;
+    const V& res;
+    TestFunW(TestFun testFun, const V& res) : testFun(testFun), res(res) {}
+    bool operator()(V range) {
+        // remove all already found radioactives from the range
+        for (const auto& r : res) {
+            range.erase(std::remove(range.begin(), range.end(), r),
+                        range.end());
+        }
+        return testFun(range);
+    }
+};
+
+void RlogN_impl(size_t low, size_t high, V& res, TestFunW& testFunW) {
+    if (low == high) {
+        //if (testFunW(range(low, low))) {
+            res.push_back(low);
+        //}
+        return;
+    }
+    if (testFunW(range(low, (low + high) / 2))) {
+        RlogN_impl(low, (low + high) / 2, res, testFunW);
+    } else {
+        RlogN_impl((low + high) / 2 + 1, high, res, testFunW);
+    }
+}
+
+V RlogN(size_t NumberOfBalls, size_t RadioactiveBalls, TestFun testFun) {
+    V res;
+    TestFunW testFunW{testFun, res};
+    assert(NumberOfBalls >= 1);
+    for (size_t i = 0; i < RadioactiveBalls; ++i) {
+        // find one r ball
+        RlogN_impl(0, NumberOfBalls - 1, res, testFunW);
+    }
+    return res;
+}
+
 std::vector<size_t> FindRadioactiveBalls(
     size_t NumberOfBalls, size_t RadioactiveBalls,
     bool (*TestFunction)(const std::vector<size_t>& BallsToTest)) {
@@ -35,7 +74,8 @@ std::vector<size_t> FindRadioactiveBalls(
         return {};
     }
 
-    return linear(NumberOfBalls, RadioactiveBalls, TestFunction);
+    // return linear(NumberOfBalls, RadioactiveBalls, TestFunction);
+    return RlogN(NumberOfBalls, RadioactiveBalls, TestFunction);
 }
 
 V impl(size_t low, size_t high, size_t& num_ractive, TestFun testFun) {
@@ -48,12 +88,12 @@ V impl(size_t low, size_t high, size_t& num_ractive, TestFun testFun) {
     }
 
     V res;
-    if (testFun(range(low, high/2))) { // ractive
-        V subres = impl(low, high/2, num_ractive, testFun);
+    if (testFun(range(low, high / 2))) { // ractive
+        V subres = impl(low, high / 2, num_ractive, testFun);
         res.insert(res.end(), subres.begin(), subres.end());
     }
-    if (num_ractive && testFun(range(high/2 + 1, high))) { // ractive
-        V subres = impl(high/2 + 1, high, num_ractive, testFun);
+    if (num_ractive && testFun(range(high / 2 + 1, high))) { // ractive
+        V subres = impl(high / 2 + 1, high, num_ractive, testFun);
         res.insert(res.end(), subres.begin(), subres.end());
     }
     return res;
--- Schrodinger/test.cpp
+++ Schrodinger/test.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include "solution.hpp"
+#include "solution.cpp"
 
 using V = std::vector<size_t>;
 
 V radioactives;
 static int called = 0;
 
+std::ostream& operator<<(std::ostream& os, const V& v) {
+    for (const auto& b : v) {
+        os << b << " ";
+    }
+    os << "\n";
+    return os;
+}
+
 /// Returns true if there is at least one radioactive ball in the set
 bool testFun(const V& ballsToTest) {
     ++called;
-    for (const auto& b: ballsToTest) {
+    std::cout << ballsToTest;
+    for (const auto& b : ballsToTest) {
         auto it = std::find(radioactives.begin(), radioactives.end(), b);
         if (it != std::end(radioactives)) {
             return true;
@@ -20,13 +29,39 @@ bool testFun(const V& ballsToTest) {
 }
 
 struct Result : ::testing::Test {
-    Result() {
-        called = 0;
-    }
+    Result() { called = 0; }
     ~Result() {
         std::cout << "TestFun called " << called << " times." << std::endl;
     }
 };
+
+TEST(Range, range) {
+    V v = range(0, 3);
+    V exp = {0, 1, 2, 3};
+    EXPECT_EQ(v, exp);
+    v = range(1, 1);
+    exp = {1};
+    EXPECT_EQ(v, exp);
+}
+
+TEST(TestFunW, trueCase) {
+    radioactives = {0, 1, 2, 3};
+    V res = {0, 3};
+    TestFunW testFunW{testFun, res};
+    EXPECT_EQ(testFunW(range(0, 3)), true);
+}
+
+TEST(TestFunW, oneInRes) {
+    radioactives = {0, 3};
+    V res = {0};
+    TestFunW testFunW{testFun, res};
+    EXPECT_EQ(testFunW(range(0, 0)), false);
+    {
+        V res = {};
+        TestFunW testFunW{testFun, res};
+        EXPECT_EQ(testFunW(range(0, 0)), true);
+    }
+}
 
 TEST_F(Result, AllRadioactive) {
     V balls = {0, 1, 2, 3};
@@ -42,15 +77,6 @@ TEST_F(Result, ZeroRadioactive) {
     auto res = FindRadioactiveBalls(balls.size(), radioactives.size(), testFun);
     std::sort(res.begin(), res.end());
     EXPECT_EQ(res, radioactives);
-}
-
-TEST(Range, range) {
-    V v = range(0,3);
-    V exp = {0,1,2,3};
-    EXPECT_EQ(v, exp);
-    v = range(1,1);
-    exp = {1};
-    EXPECT_EQ(v, exp);
 }
 
 TEST_F(Result, SizeFour) {
