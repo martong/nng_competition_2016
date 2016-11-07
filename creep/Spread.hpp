dfdc20a61c836ad3c78ea093c88f4c9aea9b1322
Peter Szabados <kangirigungi@gmail.com>
2016-11-07 21:05:52 +0100

    Use manual for instead of pointRange in iterateSpreadRadius

--- creep/Spread.hpp
+++ creep/Spread.hpp
@@ -1,15 +1,31 @@
 #ifndef CREEP_SPREAD_HPP
 #define CREEP_SPREAD_HPP
 
+#include <Point.hpp>
+
+#include <vector>
+
+inline bool isInsideCircle(Point p, int radius) {
+    // magic
+    int dx_q1=2*p.x+(0<p.x?1:-1);
+    int dy_q1=2*p.y+(0<p.y?1:-1);
+    int d2_q2=dx_q1*dx_q1+dy_q1*dy_q1;
+    return d2_q2<=radius*radius*4;
+}
+
 template<typename Function>
-void iterateSpreadArea(Point max, Point center, int radius, const Function& function) {
-    for (Point p : PointRange{
-            Point{std::max<int>(center.x - radius + 1, 0),
-                  std::max<int>(center.y - radius + 1, 0)},
-            Point{std::min<int>(center.x + radius, max.x - 1),
-                  std::min<int>(center.y + radius, max.y - 1)}}) {
-        if (isInsideCircle(p - center, radius)) {
-            function(p);
+void iterateSpreadArea(Point bound, Point center, int radius,
+        const Function& function) {
+    Point min{std::max<int>(center.x - radius + 1, 0),
+              std::max<int>(center.y - radius + 1, 0)};
+    Point max{std::min<int>(center.x + radius, bound.x - 1),
+              std::min<int>(center.y + radius, bound.y - 1)};
+    Point p;
+    for (p.y = min.y; p.y < max.y; ++p.y) {
+        for (p.x = min.x; p.x < max.x; ++p.x) {
+            if (isInsideCircle(p - center, radius)) {
+                function(p);
+            }
         }
     }
 }
--- creep/Table.hpp
+++ creep/Table.hpp
@@ -13,14 +13,6 @@ namespace MapElement {
 //  < 0: value of MapElement
 using Table = Matrix<int>;
 
-inline bool isInsideCircle(Point p, int radius) {
-    // magic
-    int dx_q1=2*p.x+(0<p.x?1:-1);
-    int dy_q1=2*p.y+(0<p.y?1:-1);
-    int d2_q2=dx_q1*dx_q1+dy_q1*dy_q1;
-    return d2_q2<=radius*radius*4;
-}
-
 inline bool hasCreep(int element) {
     return element != MapElement::Wall && element != MapElement::Floor;
 }
