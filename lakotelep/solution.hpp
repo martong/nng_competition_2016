ff2319412c15fd538786e57c143eb64a2d6e5ed2
Gabor Marton <martongabesz@gmail.com>
2016-11-08 22:02:46 +0100

    Clean up

--- lakotelep/solution.hpp
+++ lakotelep/solution.hpp
@@ -24,21 +24,15 @@ std::ostream& operator<<(std::ostream& os, std::stack<Point> ps) {
 }
 
 std::vector<Point> solve2impl(std::vector<Point>& st, Matrix<int>& m,
-                              std::vector<Point>& result,
-                              Point* parent = nullptr) {
-    //if (parent) std::cout << "parent: " << *parent << "\n";
+                              std::vector<Point>& result) {
     assert(m.size() > 0);
     if (result.size() == m.size()) {
-        // std::cout << result;
         return result;
     }
 
-    std::vector<Point> st2;
     while (!st.empty()) {
 
         auto p = st.back();
-        //std::cout << "p:" << p << "\n";
-        st2.push_back(p);
         st.pop_back();
 
         auto stc = st;
@@ -50,24 +44,19 @@ std::vector<Point> solve2impl(std::vector<Point>& st, Matrix<int>& m,
         m[p] = 0;
         result.push_back(p);
 
-        //std::vector<Point> diff;
-
         auto ns = getNeigbors(m, p);
         for (const auto& n : ns) {
             --m[n];
             if (m[n] == 0) {
                 m[n] = 4;
                 st.erase(std::remove(st.begin(), st.end(), n), st.end());
-                //auto it = std::remove(st.begin(), st.end(), n);
-                //std::copy(it, st.end(), std::back_inserter(diff));
-                //st.erase(it, st.end());
             }
             if (m[n] < 0) m[n] = 0;
             if (m[n] == 1) st.push_back(n);
         }
 
         // recurse
-        auto r = solve2impl(st, m, result, &p);
+        auto r = solve2impl(st, m, result);
         if (!r.empty()) {
             return r;
         }
@@ -78,39 +67,16 @@ std::vector<Point> solve2impl(std::vector<Point>& st, Matrix<int>& m,
             if (v >= 1) ++v;
             if (v > 4) {
                 v = 1;
-                //std::cout << "n1:" << n << "\n";
-                //auto it = std::find(st.begin(), st.end(), n);
-                // if (!st.empty() && it == std::end(st)) {
-                // st.push_back(n);
-                //}
-                //if (it == std::end(st)) {
-                    //if (st.empty()) {
-                        //auto it2 = std::find(st2.begin(), st2.end(), n);
-                        //if (it2 == std::end(st2)) {
-                            //st.push_back(n);
-                        //}
-                    //} else {
-                        //st.push_back(n);
-                    //}
-                //}
             }
         }
-        //std::copy(diff.begin(), diff.end(), std::back_inserter(st));
-
         result.pop_back();
         m[p] = 1;
         st = stc;
 
-        //if (st != stc) {
-            //std::cout << "st:" << st << "stc:" << stc << std::endl;
-        //}
-        assert(st == stc);
+        //assert(st == stc);
         assert(m == mc);
         assert(result == resultc);
     }
-    // return result;
-    //std::cout << "DEAD "
-              //<< "\n";
     return {};
 }
 
